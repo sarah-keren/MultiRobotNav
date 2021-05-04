23d5db3abd5316cf97cea0bb44f2807f088e154f
NoamKu <noam.kuehnberg@mail.huji.ac.il>
2021-05-04 19:59:32 +0000

    Refactor path_metrics to allow re-use of plans

--- scripts/path_metrics.py
+++ scripts/path_metrics.py
@@ -24,14 +24,14 @@ class PathMetrics:
         array_pose = [(item.position.x,item.position.y) for item in possible_poses_array]
         self.array_sample = random.sample(array_pose, self.k)
 	
-    def getVariance(self,goal_location):
+    def getMetricsToGoal(self,goal_location):
         goal = PoseStamped()
         goal.header.seq = 0
         goal.header.frame_id = self.ns + 'map'
         goal.header.stamp = rospy.Time(0)
         goal.pose.position.x = goal_location[0] 
         goal.pose.position.y = goal_location[1]  
-        array_lens=np.zeros(self.k)
+        size_array=np.zeros(self.k)
         
         start = PoseStamped()
         start.header.seq = 0
@@ -39,8 +39,10 @@ class PathMetrics:
         start.header.stamp = rospy.Time(0)
         start.pose.position.x = 0.5
         start.pose.position.y = -2
-        rospy.loginfo('Size of path from real start point')
-        rospy.loginfo(self.getPlanSize(start,goal))
+
+        real_size = self.getPlanMetric(start,goal)
+        rospy.loginfo('Size of path from real start point: {}'.format(real_size))
+        
         
         for i, item in enumerate(self.array_sample):
             start = PoseStamped()
@@ -49,18 +51,11 @@ class PathMetrics:
             start.header.stamp = rospy.Time(0)
             start.pose.position.x = item[0]
             start.pose.position.y = item[1]
-            array_lens[i] = self.getPlanSize(start,goal)
-
-        var=np.var(array_lens)
-        mean=np.mean(array_lens)
-
-        rospy.loginfo('Result of Path size metric:')
-        rospy.loginfo('Variance: {}'.format(var))
-        rospy.loginfo('Mean: {}'.format(mean))
-
-        return var,mean
+            size_array[i] = self.getPlanMetric(start,goal)
+        
+        self.getSizeVarianceMetric(size_array)
 	
-    def getPlanSize(self, start, goal, show=False):
+    def getPlanMetric(self, start, goal, show=False):
         get_plan_srv = self.ns + 'move_base/make_plan'
         get_plan = rospy.ServiceProxy(get_plan_srv, GetPlan)
         req = GetPlan()
@@ -71,12 +66,24 @@ class PathMetrics:
 
         if show:
             rospy.logdebug(resp.plan)
+        
+        poses_array = resp.plan.poses
+        size_metric = len(poses_array)
 
-        return len(resp.plan.poses)
+        return size_metric
+    
+    def getSizeVarianceMetric(self, size_array):
+        var=np.var(size_array)
+        mean=np.mean(size_array)
+
+        rospy.loginfo('Result of Path size metric:')
+        rospy.loginfo('Variance: {}'.format(var))
+        rospy.loginfo('Mean: {}'.format(mean))
+
 
 
 if __name__ == '__main__':
     rospy.init_node('path_length_metric')
     path_metrics = PathMetrics(ns='/tb3_0/', k=100)
-    
-    path_metrics.getVariance((1,2))
+
+    path_metrics.getMetricsToGoal((1,2))
